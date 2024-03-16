16bb03cd885ba78d43093f161f9fe88cdf9b59f3
Lennard <lennardschmale@outlook.de>
2024-03-16 22:14:55 +0100

    init

--- core/roll.py
+++ core/roll.py
@@ -1,11 +1,15 @@
 from random import randint
 
 def roll(roll):
-    rolling = []
     try:
-        for x in range(int(roll.split('d')[0])):
-            rolling.append(randint(1,int(roll.split('d')[1])))
+        num_dice, num_sides = map(int, roll.split('d'))
+        if num_dice <= 0 or num_sides <= 0:
+            raise ValueError("Number of dice and number of sides must be positive integers")
+
+        rolling = [randint(1, num_sides) for _ in range(num_dice)]
         total = sum(rolling)
         return total, rolling
+    except ValueError as err:
+        return None, f"Invalid roll format: {err}"
     except Exception as err:
         return None, str(err)
